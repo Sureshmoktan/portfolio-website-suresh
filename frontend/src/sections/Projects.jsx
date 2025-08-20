9c43f83f704cef9bc7ebeef063e1b1e3c9c80bf6
Suresh Moktan <moktansuresh931@gmail.com>
2025-08-20 09:09:16 +0545

    changed  the project path

--- frontend/src/sections/Projects.jsx
+++ frontend/src/sections/Projects.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { projects } from "../data/projectsData";
+import { projects } from "../data/projectsData.js";
 
 export default function Projects() {
   return (
