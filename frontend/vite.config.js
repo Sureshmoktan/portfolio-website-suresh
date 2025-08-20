5c129126d705f4e054aa688e6b71dbda548146f2
Suresh Moktan <moktansuresh931@gmail.com>
2025-08-20 08:56:11 +0545

    package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-website-suresh/',   // 👈 add this line
   plugins: [
     react(),
     tailwindcss(),
