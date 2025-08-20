185bf1547e9e9cfc303ef484bb40cb266f0ecefc
Suresh Moktan <moktansuresh931@gmail.com>
2025-08-20 09:14:39 +0545

    rename file projects

--- frontend/src/sections/Projects.jsx
+++ frontend/src/sections/Projects.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { projects } from "../data/projectsData.js";
+import { projects } from "../data/ProjectsData.js";
 
 export default function Projects() {
   return (
